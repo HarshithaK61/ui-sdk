a80bfb0a6dac53bfb3f06b734cf4bc321fd620ef
Harshitha <harshitha@creativeideaz.io>
2026-09-23 22:35:26 +0530

    Clean deploy without worker

---
+++ _nuxt/BVWtygp9.js
@@ -0,0 +1,1 @@
+import{M as e}from"./qUUFd7It.js";import{d as o,F as r,o as s,L as a}from"./By2ZLYYu.js";const m=o({__name:"your-ids",setup(t){return(n,c)=>(s(),r(a(e)))}});export{m as default};
---
+++ _nuxt/KW7zN_4Q.js
@@ -0,0 +1,1 @@
+function i(){return/iPad|iPhone|iPod/.test(navigator.userAgent)&&!window.MSStream}function e(){return/android/i.test(navigator.userAgent)}export{e as a,i};
